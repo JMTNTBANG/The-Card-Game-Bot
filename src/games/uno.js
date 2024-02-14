50ed75ae971ce5353f6b5e61a8e1c69077d79e2a
James Miller <32822936+JMTNTBANG@users.noreply.github.com>
2024-02-13 23:31:45 -0600

    Added Hand Display
    Player hands now get displayed in their respective private threads on game creation

--- src/games/uno.js
+++ src/games/uno.js
@@ -2,7 +2,22 @@ const fs = require("fs");
 const { uno_deck } = require("./static.json");
 const { ChannelType, ThreadAutoArchiveDuration } = require("discord.js");
 
+async function show_hands(game) {
+  for (player of game.players) {
+    let message = "Current Cards:\n";
+    for (card of player.hand) {
+      if (card.type == "number" && card.color != "wild") {
+        message += `\`${card.color[0]}${card.number}\` `;
+      } else if (card.number == -1 && card.color == "wild") {
+        message += `\`${card.color}\` `;
+      } else {
+        message += `\`${card.color} ${card.type}\` `;
+      }
+    }
+    await player.thread.send(message);
+  }
 }
+
 module.exports = {
   async start_game(lobby, owner) {
     const configFile = JSON.parse(fs.readFileSync("./src/config.json"));
@@ -19,16 +34,18 @@ module.exports = {
       }),
     };
     for (player of lobby.embeds[0].data.fields[0].value.split("\n")) {
-      const playerDiscord = await lobby.guild.members.fetch(player.slice(2, -1))
+      const playerDiscord = await lobby.guild.members.fetch(
+        player.slice(2, -1)
+      );
       const new_player = {
         id: player.slice(2, -1),
         hand: [],
         thread: await game.channel.threads.create({
           name: playerDiscord.displayName,
-          type: ChannelType.PrivateThread
-        })
+          type: ChannelType.PrivateThread,
+        }),
       };
-      new_player.thread.send(`<@${new_player.id}>`)
+      new_player.thread.send(`<@${new_player.id}>`);
       for (let i = 0; i < 7; i++) {
         const card = Math.floor(Math.random() * game.deck.length);
         new_player.hand.push(game.deck[card]);
@@ -44,5 +61,6 @@ module.exports = {
       "./src/active-games.json",
       JSON.stringify(activeGames, "", 2)
     );
+    await show_hands(game);
   },
 };
