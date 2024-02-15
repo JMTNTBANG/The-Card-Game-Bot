50329371b89bfb5ce0fcf0413cf502c17428622c
James Miller <32822936+JMTNTBANG@users.noreply.github.com>
2024-02-15 00:10:33 -0600

    Added Functionality for Special Cards
    Added Functionality for Wilds, Draws, Reverses, and Skips, a command for drawing a card was also added. A Few bugs were also fixed.
    
    The UNO Game is in basic working condition and can support full UNO Games, as shown in this [discord channel](https://discord.com/channels/1205013098671640586/1207565316012646461)

--- src/games/uno.js
+++ src/games/uno.js
@@ -5,6 +5,8 @@ const { ChannelType } = require("discord.js");
 function print_card(card) {
   if (card.type == "number" && card.color != "wild") {
     return card.color[0] + card.number;
+  } else if (card.type == "draw" && card.color == "wild") {
+    return card.color + card.type
   } else if (card.number == -1 && card.color == "wild") {
     return card.color;
   } else {
@@ -12,42 +14,126 @@ function print_card(card) {
   }
 }
 
+function draw_card(amt, hand, deck) {
+  for (let i = 0; i < amt; i++) {
+    const card = Math.floor(Math.random() * deck.length);
+    hand.push(deck[card]);
+    deck.splice(card, 1);
+  }
+  return hand;
+}
+
 async function show_current_card(game) {
+  var card = print_card(game.current_card);
+  if (game.current_card.number == -2) {
+    card = `wild ${card.color}`;
+  }
   const currentPlayerDiscord = await game.guild.members.fetch(
     game.players[game.current_turn].id
   );
   await game.channel.send(
-    `# New Turn\n## Current Card:\n\`${print_card(
-      game.current_card
-    )}\`\n## Current Player:\n${currentPlayerDiscord.toString()}`
+    `# New Turn\n## Current Card:\n\`${card}\`\n## Current Player:\n${currentPlayerDiscord.toString()}`
   );
 }
 
-async function show_hands(game) {
-  for (player of game.players) {
-    let message = "Current Cards:\n";
-    for (card of player.hand) {
-      message += `\`${print_card(card)}\` `;
-    }
-    await player.thread.send(message);
-  }
+async function show_hands(game, definedPlayer = undefined) {
+  game.players
+    .filter(
+      (player) => definedPlayer == undefined || player.id == definedPlayer.id
+    )
+    .forEach(async (player) => {
+      let message = "Current Cards:\n";
+      player.hand.forEach((card) => {
+        message += `\`${print_card(card)}\` `;
+      });
+      await player.thread.send(message);
+    });
 }
 
 async function sent_thread_cmd(game, message) {
   const player = game.players.filter(
     (player) =>
-      player.id == message.author.id && player.thread.id == message.channel.id
+      player.id == message.author.id &&
+      player.thread.id == message.channel.id &&
+      game.current_turn == game.players.indexOf(player)
   )[0];
+  if (message.content == "draw") {
+    var hand = draw_card(1, player.hand, game.deck);
+    hand
+      .filter((card) => !player.hand.includes(card))
+      .forEach((card) => {
+        game.deck.splice(hand.indexOf(card), 1);
+      });
+    player.hand = hand;
+    await show_hands(game, player);
+  } else;
   const cards = player.hand.filter(
-    (card) => print_card(card) == message.content
+    (card) => message.content.startsWith(print_card(card))
   );
   const card = cards.filter(
     (card) =>
       card.color == game.current_card.color ||
       card.number == game.current_card.number ||
-      card.color == "wild"
+      card.color == "wild" ||
+      game.current_card.color == "wild"
   )[0];
   if (card == null) return;
+  if (card.color == "wild") {
+    card.number = -2;
+    if (message.content.endsWith("red")) {
+      card.color = "red";
+    } else if (message.content.endsWith("yellow")) {
+      card.color = "yellow";
+    } else if (message.content.endsWith("green")) {
+      card.color = "green";
+    } else if (message.content.endsWith("blue")) {
+      card.color = "blue";
+    } else return;
+    if (card.type == "draw") {
+      var next_player = game.current_turn;
+      if (next_player + 1 < game.players.length) {
+        next_player += 1;
+      } else {
+        next_player = 0;
+      }
+      var hand = draw_card(2, game.players[next_player].hand, game.deck);
+      hand
+        .filter((card) => !game.players[next_player].hand.includes(card))
+        .forEach((card) => {
+          game.deck.splice(hand.indexOf(card), 1);
+        });
+      game.players[next_player].hand = hand;
+      game.current_turn = next_player;
+    }
+  }
+  switch (card.type) {
+    case "skip":
+      if (game.current_turn + 1 < game.players.length) {
+        game.current_turn += 1;
+      } else {
+        game.current_turn = 0;
+      }
+      break;
+    case "reverse":
+      game.players.reverse();
+      break;
+    case "draw":
+      var next_player = game.current_turn;
+      if (next_player + 1 < game.players.length) {
+        next_player += 1;
+      } else {
+        next_player = 0;
+      }
+      var hand = draw_card(2, game.players[next_player].hand, game.deck);
+      hand
+        .filter((card) => !game.players[next_player].hand.includes(card))
+        .forEach((card) => {
+          game.deck.splice(hand.indexOf(card), 1);
+        });
+      game.players[next_player].hand = hand;
+      game.current_turn = next_player;
+      break;
+  }
   game.current_card = card;
   player.hand.splice(player.hand.indexOf(card), 1);
   if (game.current_turn + 1 < game.players.length) {
@@ -90,11 +176,13 @@ module.exports = {
         }),
       };
       new_player.thread.send(`<@${new_player.id}>`);
-      for (let i = 0; i < 7; i++) {
-        const card = Math.floor(Math.random() * game.deck.length);
-        new_player.hand.push(game.deck[card]);
-        game.deck.splice(card, 1);
-      }
+      var hand = draw_card(7, [], game.deck);
+      hand
+        .filter((card) => !new_player.hand.includes(card))
+        .forEach((card) => {
+          game.deck.splice(hand.indexOf(card), 1);
+        });
+      new_player.hand = hand;
       game.players.push(new_player);
     }
     const card = Math.floor(Math.random() * game.deck.length);
