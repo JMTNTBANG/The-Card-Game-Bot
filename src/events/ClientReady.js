8d354955480fa090a2086719a6b425756fba90cd
James Miller <32822936+JMTNTBANG@users.noreply.github.com>
2024-02-22 21:38:50 -0600

    Added Patron Status
    Shows all the patrons 1 by 1 in the Status of the bot every 10 seconds

--- src/events/ClientReady.js
+++ src/events/ClientReady.js
@@ -1,9 +1,42 @@
 const { Events } = require("discord.js");
+const { patreonToken, defaultStatus } = require("../config.json");
+const { patreon } = require("patreon");
+const patreonClient = patreon(patreonToken);
+
+function patronsInStatus(ctx) {
+  setInterval(function () {
+  const statuses = [defaultStatus];
+  patreonClient("/current_user/campaigns").then(async ({ store }) => {
+    const campaign = store
+      .findAll("campaign")
+      .map((campaign) => campaign.serialize());
+    patreonClient(
+      `/campaigns/${campaign[0].data.id}/pledges?include=patron.null`
+    ).then(async ({ store }) => {
+      const pledges = store
+        .findAll("campaign")
+        .map((pledges) => pledges.serialize());
+      if (pledges.included) {
+        pledges.included.forEach((pledge) => {
+          statuses.push(`Thanks ${pledge.attributes.full_name}!`);
+        });
+      }
+    });
+  });
+    statuses.forEach((status) => {
+      setTimeout(function () {
+        ctx.user.setActivity({
+          name: status,
+        });
+      }, 10_000);
+    });
+  }, 10_000);
+}
 
 module.exports = {
   name: Events.ClientReady,
   async execute(ctx) {
-    ctx.user.setActivity("UNO in Discord!");
+    patronsInStatus(ctx)
     console.log(`Logged in as ${ctx.user.tag}`);
   },
 };
